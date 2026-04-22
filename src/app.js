2644e3491304da77c8e48e0ebad489f9442e54c5
arb-pdf-converter <mahmodabusableh@gmail.com>
2026-04-22 17:51:37 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,6 +22,25 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+
+
+app.get("/test-gs", async (req, res) => {
+  const { exec } = require("child_process");
+
+  exec("gs --version", (err, stdout, stderr) => {
+    if (err) {
+      return res.status(500).send({
+        error: "Ghostscript NOT working",
+        details: err.message,
+      });
+    }
+
+    res.send({
+      success: true,
+      version: stdout,
+    });
+  });
+});
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
